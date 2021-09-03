84b35eb4993c22be71dcd092f0e80724167f85ee
Perry <dannoehe@dannoetekiMacBook-Pro.local>
2021-09-03 16:15:54 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const app = require("express")();
-const PORT = 4000;
+const PORT = 3000;
 app.get("", (req, resp) => {
     resp.send("Hello Perry!");
 });
 
-app.listen(4000, () => {
-    console.log('app is on port 4000');
+app.listen(3000, () => {
+    console.log('app is on port 3000');
 });
